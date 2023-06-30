3f957f8051dae89c0d87d2f511e5e8a5cf63e517
MaxGolovan <126692734+MaxGolovan@users.noreply.github.com>
2023-06-30 13:08:05 +0300

    Add files via upload

---
+++ rustlings/variables/variables5.rs
@@ -0,0 +1,11 @@
+// variables5.rs
+// Execute `rustlings hint variables5` or use the `hint` watch subcommand for a hint.
+
+
+
+fn main() {
+    let number_text = "T-H-R-E-E"; 
+    println!("Spell a Number: {}", number_text);
+    let number = 3; 
+    println!("Number plus two is: {}", number + 2);
+}
